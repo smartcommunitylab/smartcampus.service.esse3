ea26444ea7728e3b247e327d066b186dd739bb24
adami <adami@0fd5999a-94d0-4028-93f2-09691c278cb2>
2012-09-14 09:07:03 +0000

    Build an "empty" proto if the json input is an empty string.

--- src/main/java/smartcampus/service/esse3/script/ADAConnectionScript.java
+++ src/main/java/smartcampus/service/esse3/script/ADAConnectionScript.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.TreeMap;
 
 import org.codehaus.jackson.JsonParseException;
 import org.codehaus.jackson.map.JsonMappingException;
@@ -19,20 +20,25 @@ public class ADAConnectionScript {
 		Student.Builder builder = Student.newBuilder();
 		
 		ObjectMapper mapper = new ObjectMapper();
-		Map<String, Object> map = mapper.readValue(json, Map.class);
+		Map<String, Object> map;
+		if (json== null || json.length() == 0) {
+			map = new TreeMap<String, Object>();
+		} else {
+			map = mapper.readValue(json, Map.class);
+		}
 		
-		builder.setNation((String)map.get("nation"));
-		builder.setPhone((String)map.get("phone"));
-		builder.setMobile((String)map.get("mobile"));
-		builder.setAddress((String)map.get("address"));
-		builder.setEnrollmentYear((String)map.get("enrollmentYear"));
-		builder.setAcademicYear((String)map.get("academicYear"));
-		builder.setSupplementaryYears((String)map.get("supplementaryYears"));
-		builder.setCfu((String)map.get("cfu"));
-		builder.setCfuTotal((String)map.get("cfuTotal"));
-		builder.setMarksNumber((String)map.get("marksNumber"));
-		builder.setMarksAverage((String)map.get("marksAverage"));
-		builder.setCds((String)map.get("cds"));
+		builder.setNation(notNull((String)map.get("nation")));
+		builder.setPhone(notNull((String)map.get("phone")));
+		builder.setMobile(notNull((String)map.get("mobile")));
+		builder.setAddress(notNull((String)map.get("address")));
+		builder.setEnrollmentYear(notNull((String)map.get("enrollmentYear")));
+		builder.setAcademicYear(notNull((String)map.get("academicYear")));
+		builder.setSupplementaryYears(notNull((String)map.get("supplementaryYears")));
+		builder.setCfu(notNull((String)map.get("cfu")));
+		builder.setCfuTotal(notNull((String)map.get("cfuTotal")));
+		builder.setMarksNumber(notNull((String)map.get("marksNumber")));
+		builder.setMarksAverage(notNull((String)map.get("marksAverage")));
+		builder.setCds(notNull((String)map.get("cds")));
 		
 		// fillers : missing
 		builder.setFiscalCode("");
@@ -50,7 +56,12 @@ public class ADAConnectionScript {
 		List<Exam> result = new ArrayList<Exam>();
 		
 		ObjectMapper mapper = new ObjectMapper();
-		List<Object> list = mapper.readValue(json, List.class);
+		List<Object> list;
+		if (json== null || json.length() == 0) {
+			list = new ArrayList<Object>();
+		} else {
+			list = mapper.readValue(json, List.class);
+		}
 		
 		for (Object e: list) {
 			Map<String, Object> map = mapper.convertValue(e, Map.class);
@@ -60,9 +71,17 @@ public class ADAConnectionScript {
 			System.out.println(map);
 			
 			builder.setCod(notNull((String)map.get("cod")));
-			builder.setDate((Long)map.get("date"));
+			if (map.containsKey("date")) {
+				builder.setDate((Long)map.get("date"));
+			} else {
+				builder.setDate(Long.MIN_VALUE);
+			}
 			builder.setId(notNull((String)map.get("id")));
-			builder.setLode((Boolean)map.get("lode"));
+			if (map.containsKey("lode")) {
+				builder.setLode((Boolean)map.get("lode"));
+			} else {
+				builder.setLode(false);
+			}
 			builder.setName(notNull((String)map.get("name")));
 			builder.setResult(notNull((String)map.get("result")));
 			builder.setWeight(notNull(((Integer)map.get("weight")).toString()));
